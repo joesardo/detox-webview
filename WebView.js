89ebadefdf112666b98a8fdeae17dc152df44cdc
Joseph Sardo <josephs5526@gmail.com>
2024-03-02 17:54:14 -0500

    add webview class

---
+++ WebView.js
@@ -0,0 +1,34 @@
+const detox = require('detox');
+const { device, expect, element, by } = detox;
+
+class DetoxWebView {
+  async switchToWebView(webViewIdentifier) {
+    await device.launchApp({ newInstance: false });
+    await device.reloadReactNative();
+
+    await device.waitFor(element(by.id(webViewIdentifier))).toBeVisible().withTimeout(5000);
+
+    const webViewHandles = await element.all(by.webView()).get();
+    const lastWebViewHandle = webViewHandles[webViewHandles.length - 1];
+
+    await device.element(lastWebViewHandle).tap();
+  }
+
+  async tapElementInWebView(elementLocator) {
+    await element(by.webView()).tap(elementLocator);
+  }
+
+  async typeTextInWebView(text, elementLocator) {
+    await element(by.webView()).typeText(text, elementLocator);
+  }
+
+  async getTextFromWebView(elementLocator) {
+    return await element(by.webView()).getText(elementLocator);
+  }
+
+  async waitForWebViewElement(elementLocator, timeout = 10000) {
+    await element(by.webView()).waitFor(element(by.id(elementLocator))).toBeVisible().withTimeout(timeout);
+  }
+}
+
+module.exports = new DetoxWebView();
